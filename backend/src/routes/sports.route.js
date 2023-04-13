a0a08b8536e91797ce1773fa09e552d2df781f57
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-13 18:26:21 +0530

    Imported: updateEvent from controller

--- backend/src/routes/sports.route.js
+++ backend/src/routes/sports.route.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
-const {getEvents, createEvent, deleteEvent} = require("../controllers/sports.controller")
+const {getEvents, createEvent, deleteEvent, updateEvent} = require("../controllers/sports.controller")
 
 router.get("/events", getEvents)
 router.post("/events/create", createEvent)
+router.patch("/events/update/:id", updateEvent)
 router.delete("/events/delete/:id", deleteEvent)
 
 module.exports = router
