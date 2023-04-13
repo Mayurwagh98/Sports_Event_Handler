96bb9ba1faa285575235c4fab8348ce7651fb2df
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-13 17:28:16 +0530

    Changed: login from email, pass to username, pass

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -5,23 +5,24 @@ const jwt = require("jsonwebtoken")
 
 let Signup = async(req, res) =>{
     
-    let {name, email, password} = req.body
+    let {username, email, password} = req.body
 
     let user = await User.findOne({email})
     try {
         
         if(user){
     
-            res.status(200).send({message: "User already exists"})
+            return res.status(200).send({message: "User already exists"})
         }
     
         const hashPassword = await bcrypt.hash(password, 8)
     
-        const newUser = await User.create({name, email, password: hashPassword})
+        const newUser = await User.create({username, email, password: hashPassword})
     
         const token = jwt.sign({email: newUser.email}, 'blah')
     
-        res.status(200).send({message: "User registered", newUser, token})
+        return res.status(200).send({message: "User registered", newUser, token})
+
     } catch (error) {
         console.log(error.message)   
     }
@@ -30,27 +31,27 @@ let Signup = async(req, res) =>{
 
 let Login = async(req, res) =>{
 
-    let {email, password} = req.body
+    let {username, password} = req.body
 
-    let user = await User.findOne({email})
+    let user = await User.findOne({username})
     try {
 
         if(!user){
-            res.status(400).send({message: "User doesn't exists"})
+            return res.status(400).send({message: "User doesn't exists"})
         }
 
         const matchPassword = await bcrypt.compare(password, user.password)
 
         if(!matchPassword){
-            res.status(404).send({message: "Wrong Credentials"})
+            return res.status(404).send({message: "Wrong Credentials"})
         }
 
         const token = jwt.sign({userID: user._id},'blah')
 
-        res.status(200).send({message: "Login Successful!", token: token})
+        return res.status(200).send({message: "Login Successful!", token: token})
         
     } catch (error) {
-        res.status(500).send({message: error. message})
+        return res.status(500).send({message: error. message})
     }
 }
 
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-    name:String,
+    username:String,
     email:String,
     password:String
 })
