dca70bce5ac36f0e420fc90dfd0b2457d4b95a54
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-15 17:36:26 +0530

    Added: image in the modal

--- backend/src/models/sports.model.js
+++ backend/src/models/sports.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 
 const SportsSchema = mongoose.Schema({
     title:String,
+    image: String,
     description: String,
     timing: String,
     number_of_players_limit: Number,
